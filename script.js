b65d4eb4a4ddb6f71b99811b460d8ae8ba8af712
Brooks <brooks@zionhollow.com>
2025-03-04 20:06:07 -0500

    Added Footer to Page

--- script.js
+++ script.js
@@ -78,3 +78,5 @@ function sendHeight() {
 // Run on page load & window resize
 window.addEventListener("load", sendHeight);
 window.addEventListener("resize", sendHeight);
+
+document.getElementById("currentYear").textContent = new Date().getFullYear();
